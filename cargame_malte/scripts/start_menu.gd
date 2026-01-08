f88f9953e7eafe0ce03f020c40034ae89dc9ba38
Maltel71 <156253099+Maltel71@users.noreply.github.com>
2026-01-08 11:37:01 +0100

    a lot of fixes

---
+++ cargame_malte/scripts/start_menu.gd
@@ -0,0 +1,42 @@
+# start_menu.gd
+extends Control
+
+@export var game_scene_path: String = "res://levels/car_level_1.tscn"  # Update this path
+@export var hover_sound: AudioStream
+@export_range(-80, 24) var hover_volume: float = 0.0
+
+@onready var play_button = $Panel/VBoxContainer/PlayButton  # Renamed
+@onready var settings_button = $Panel/VBoxContainer/SettingsButton
+@onready var quit_button = $Panel/VBoxContainer/QuitButton
+@onready var audio_player = $AudioStreamPlayer
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+	if audio_player:
+		audio_player.bus = "SFX"
+	
+	play_button.pressed.connect(_on_play_pressed)  # Renamed
+	settings_button.pressed.connect(_on_settings_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
+	
+	if hover_sound:
+		play_button.mouse_entered.connect(_on_button_hover)
+		settings_button.mouse_entered.connect(_on_button_hover)
+		quit_button.mouse_entered.connect(_on_button_hover)
+
+func _on_play_pressed():  # Renamed
+	if game_scene_path != "":
+		get_tree().change_scene_to_file(game_scene_path)
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://menus/settings_menu.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_button_hover():
+	if hover_sound and audio_player:
+		audio_player.stream = hover_sound
+		audio_player.volume_db = hover_volume
+		audio_player.play()
