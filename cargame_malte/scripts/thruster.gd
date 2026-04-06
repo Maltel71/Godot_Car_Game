f40ed8791d21018996f2c81cadecc3da31e57ca7
Maltel71 <156253099+Maltel71@users.noreply.github.com>
2026-04-06 18:25:35 +0200

    thruster veichle test

--- cargame_malte/scripts/thruster.gd
+++ cargame_malte/scripts/thruster.gd
@@ -3,10 +3,10 @@ extends Node3D
 @export var thrust_force: float = 5000.0
 @export var particle_effect: GPUParticles3D
 @export var puff_effect: GPUParticles3D
+@export var activate_action: String = "ui_select"  # Set per-instance in the Inspector
 
 var parent_body: VehicleBody3D
-var powerup_active: bool = false
-var powerup_timer: float = 0.0
+var thruster_enabled: bool = false
 var was_thrusting: bool = false
 
 func _ready():
@@ -15,45 +15,38 @@ func _ready():
 	while node and not node is VehicleBody3D:
 		node = node.get_parent()
 	parent_body = node as VehicleBody3D
-	
+
 	visible = false
 	if particle_effect:
 		particle_effect.emitting = false
-	set_physics_process(false)
-		
-func activate_powerup(duration: float):
-	visible = true
-	powerup_active = true
-	powerup_timer = duration
 	set_physics_process(true)
+
+func _input(event):
+	# Toggle thruster on/off with the assigned button
+	if event.is_action_pressed("thruster_toggle"):
+		thruster_enabled = not thruster_enabled
+		visible = thruster_enabled
+		if not thruster_enabled and particle_effect:
+			particle_effect.emitting = false
 
 func _physics_process(delta):
 	if not parent_body:
 		return
-	
-	# Update powerup timer
-	if powerup_active:
-		powerup_timer -= delta
-		if powerup_timer <= 0.0:
-			powerup_active = false
-			visible = false
-			set_physics_process(false)
-	
-	# Thrusters active if powerup AND space pressed
-	var should_thrust = powerup_active and Input.is_action_pressed("ui_select")
-	
+
+	# Hold the assigned action to thrust (only when toggled on)
+	var should_thrust = thruster_enabled and Input.is_action_pressed(activate_action)
+
 	if should_thrust:
-		# Play puff effect when starting to thrust
 		if not was_thrusting and puff_effect:
 			puff_effect.restart()
-		
+
 		var thrust_direction = global_transform.basis.y
 		parent_body.apply_force(thrust_direction * thrust_force, global_position - parent_body.global_position)
-		
+
 		if particle_effect:
 			particle_effect.emitting = true
 	else:
 		if particle_effect:
 			particle_effect.emitting = false
-	
+
 	was_thrusting = should_thrust
