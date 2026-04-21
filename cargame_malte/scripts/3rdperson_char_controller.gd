8a373b706c140f666362b28ae018806ec2af6b85
Maltel71 <156253099+Maltel71@users.noreply.github.com>
2026-04-21 22:11:35 +0200

    3rd person implementation walk

---
+++ cargame_malte/scripts/3rdperson_char_controller.gd
@@ -0,0 +1,40 @@
+extends CharacterBody3D
+
+@export var walk_speed: float = 3.0
+@export var run_speed: float = 6.0
+@export var jump_velocity: float = 5.0
+@export var sens_h: float = 0.2
+@export var sens_v: float = 0.2
+
+@onready var camera_mount: Node3D = $camera_mount
+@onready var visuals: Node3D = $visuals
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sens_h))
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y * sens_v))
+		camera_mount.rotation.x = clamp(camera_mount.rotation.x, deg_to_rad(-60), deg_to_rad(20))
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	var speed = run_speed if Input.is_action_pressed("run") else walk_speed
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		visuals.look_at(position + direction)
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
