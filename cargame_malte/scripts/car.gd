f9c3c8a650eb65b827bb90865986c9d75c82d1db
Maltel71 <156253099+Maltel71@users.noreply.github.com>
2026-04-22 18:58:31 +0200

    car flip start impl

--- cargame_malte/scripts/car.gd
+++ cargame_malte/scripts/car.gd
@@ -1,5 +1,4 @@
 extends VehicleBody3D
-
 var max_RPM = 900
 var max_torque = 600
 var turn_speed = 3
@@ -15,12 +14,20 @@ var is_dead: bool = false
 @export var exhaust_particles: GPUParticles3D
 @export var explosion_particles: GPUParticles3D
 @export var rpm_threshold: float = 50.0
+@export_group("Flip")
+@export var flip_up_impulse: float = 8.0
+@export var flip_torque_impulse: float = 5.0
+@export var flip_cooldown: float = 2.0
+var _flip_timer: float = 0.0
 
 func _physics_process(delta):
 	if is_dead:
 		return
 	
 	$CamArm.position = position
+	
+	_try_flip()
+	_flip_timer -= delta
 	
 	var dir = Input.get_action_strength("Gas") - Input.get_action_strength("Reverse")
 	var steering_dir = Input.get_action_strength("Left") - Input.get_action_strength("Right")
@@ -50,6 +57,16 @@ func _physics_process(delta):
 		angular_velocity *= air_control_damping
 	else:
 		air_time = 0.0
+
+func _try_flip():
+	var is_upright = global_transform.basis.y.dot(Vector3.UP) > 0.8
+	if is_upright:
+		return
+	
+	if Input.is_action_just_pressed("flip") and _flip_timer <= 0.0:
+		apply_central_impulse(Vector3.UP * flip_up_impulse * mass)
+		apply_torque_impulse(global_transform.basis.z * flip_torque_impulse * mass)
+		_flip_timer = flip_cooldown
 
 func explode() -> void:
 	is_dead = true
