7a16ca9aa8b217eb04f1c50109799f757cf82b15
Maltel71 <156253099+Maltel71@users.noreply.github.com>
2026-04-23 18:46:14 +0200

    lektion9

---
+++ cargame_malte/scripts/new_scriptlite.gd
@@ -0,0 +1,6 @@
+extends SpotLight3D
+
+func _input(lampknapp):
+	if lampknapp is InputEventMouseButton and lampknapp.button_index == MOUSE_BUTTON_RIGHT and lampknapp.pressed:
+		visible = !visible
+	
