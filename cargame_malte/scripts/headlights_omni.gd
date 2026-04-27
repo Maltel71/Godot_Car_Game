8f43a21774208a6367675be543b2b8b8694ac555
Maltel71 <156253099+Maltel71@users.noreply.github.com>
2026-04-27 09:23:22 +0200

    headlights fixed

--- cargame_malte/scripts/headlights_omni.gd
+++ cargame_malte/scripts/headlights_omni.gd
@@ -2,4 +2,9 @@ extends OmniLight3D
 
 func _input(event):
 	if event.is_action_pressed("headlight"):
+		var car = get_parent()
+		while car and not car is VehicleBody3D:
+			car = car.get_parent()
+		if car and "driver_in_car" in car and not car.driver_in_car:
+			return
 		visible = !visible
